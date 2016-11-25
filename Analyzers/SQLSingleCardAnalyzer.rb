a56bb5b0bd0e6938f5284b031ea8243237c3aec7
Peter Xin <xinguangyao@gmail.com>
2016-11-25 10:42:18 +0800

    Temp code removed.

--- Analyzers/SQLSingleCardAnalyzer.rb
+++ Analyzers/SQLSingleCardAnalyzer.rb
@@ -207,7 +207,6 @@ class SQLSingleCardAnalyzer < AnalyzerBase
 			logger.warn "try to translate a pg_result: nil"
 			return {}
 		end
-		logger.error pg_result.inspect
 		pg_result.map { |piece| add_extra_message(piece); piece }
 	end
 	
