2416240baa11a87950c2db252737201c415d27bf
Peter Xin <xinguangyao@gmail.com>
2016-12-11 13:40:14 +0800

    Fix

--- Analyzers/Analyzer.rb
+++ Analyzers/Analyzer.rb
@@ -148,6 +148,7 @@ module Analyzer
 		else
 			logger.info "Received heartbeat."
 			time = params["time"]
+			time = Time.now if tme == nil
 			if $analyzer_heartbeat_thread == nil
 				$analyzer_heartbeat_thread = Thread.new do
 					Analyzer.heartbeat time
